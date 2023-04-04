7068564216554d178521cad4897cacd4e7b4733c
Martin <s182935@student.dtu.dk>
2023-04-04 19:09:57 +0200

    Evaluation bar and tests

--- ScraperTest.cs
+++ ScraperTest.cs
@@ -20,6 +20,7 @@ namespace Test
         GameScraper gameScraper;
         ChessBoard board = new ChessBoard();
         LastMoveIcon lastMoveIcon;
+        EvaluationBar evalBar;
         public ScraperTest() 
         {
             var driver = new ChromeDriver();
@@ -77,6 +78,20 @@ namespace Test
 
         }
 
+        [Fact]
+        public void evalBarTest()
+        {
+            gameScraper.FindPlayerColor();
+            this.evalBar = new EvaluationBar(scraper.driver);
+
+            evalBar.CreateBar(gameScraper.isWhite);
+            evalBar.UpdateBar(gameScraper.isWhite, 2000, null);
+            evalBar.UpdateBar(gameScraper.isWhite, 0, null);
+            evalBar.UpdateBar(gameScraper.isWhite, 3600, null);
+            evalBar.UpdateBar(gameScraper.isWhite, -4000, null);
+            evalBar.UpdateBar(gameScraper.isWhite, -8000, null);
+            evalBar.UpdateBar(gameScraper.isWhite, 0, null);
+        }
 
 
     }
